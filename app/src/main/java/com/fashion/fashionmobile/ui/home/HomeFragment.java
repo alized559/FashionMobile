24244c68fa0d1ba4889961601836d4b71911501e
Ali Zein Al Dine <alized559@gmail.com>
2022-05-14 15:14:09 +0300

    edit popular products

--- app/src/main/java/com/fashion/fashionmobile/ui/home/HomeFragment.java
+++ app/src/main/java/com/fashion/fashionmobile/ui/home/HomeFragment.java
@@ -1,13 +1,19 @@
 package com.fashion.fashionmobile.ui.home;
 
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
 
@@ -20,6 +26,7 @@ import com.android.volley.toolbox.Volley;
 import com.fashion.fashionmobile.R;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
@@ -27,10 +34,13 @@ import androidx.navigation.Navigation;
 import com.fashion.fashionmobile.databinding.FragmentHomeBinding;
 import com.fashion.fashionmobile.helpers.ImageCache;
 import com.fashion.fashionmobile.helpers.ServerUrls;
+import com.google.android.flexbox.FlexDirection;
 import com.google.android.flexbox.FlexboxLayout;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.text.DecimalFormat;
 
 public class HomeFragment extends Fragment {
 
@@ -38,6 +48,7 @@ public class HomeFragment extends Fragment {
     private FlexboxLayout flex;
     private ViewFlipper flipper;
     private RequestQueue queue;
+    private static final DecimalFormat df = new DecimalFormat("0.00");
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -63,22 +74,33 @@ public class HomeFragment extends Fragment {
 
     public void getPopularProducts(View root) {
         JsonArrayRequest request = new JsonArrayRequest(ServerUrls.getPopularProducts, new Response.Listener<JSONArray>() {
+            @RequiresApi(api = Build.VERSION_CODES.P)
             @Override
             public void onResponse(JSONArray response) {
                 try {
                     for (int i = 0; i < response.length(); i++) {
                         JSONObject row = response.getJSONObject(i);
                         int prod_id = row.getInt("prod_id");
+                        String name = row.getString("name");
+                        double price = row.getDouble("price");
+                        double discount = row.getDouble("discount");
                         if(ImageCache.GetProductImage(prod_id) == null) {
                             ImageRequest request = new ImageRequest(ServerUrls.getProductImage(prod_id), new Response.Listener<Bitmap>() {
                                 @Override
                                 public void onResponse(Bitmap response) {
+                                    LinearLayout linear = new LinearLayout(root.getContext());
+                                    LinearLayout.LayoutParams linearlp = new LinearLayout.LayoutParams(ViewGroup.
+                                            LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                                    linearlp.setMargins(0, 0, 0, 60);
+                                    linear.setOrientation(LinearLayout.VERTICAL);
+                                    linear.setLayoutParams(linearlp);
+
                                     CardView card = new CardView(root.getContext());
                                     LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.
                                             LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                                    lp.setMargins(0, 0, 0, 60);
                                     card.setLayoutParams(lp);
                                     card.setRadius((int) (10 * root.getContext().getResources().getDisplayMetrics().density + 0.5f));
+
                                     ImageView img = new ImageView(root.getContext());
                                     img.setImageBitmap(response);
                                     LinearLayout.LayoutParams lp1 = new LinearLayout.LayoutParams(
@@ -87,8 +109,71 @@ public class HomeFragment extends Fragment {
                                     img.setLayoutParams(lp1);
                                     img.setScaleType(ImageView.ScaleType.CENTER_CROP);
                                     ImageCache.SetProductImage(prod_id, response);
+
                                     card.addView(img);
-                                    flex.addView(card);
+
+                                    LinearLayout.LayoutParams textLayout = new LinearLayout.LayoutParams(
+                                            (int) (155 * root.getContext().getResources().getDisplayMetrics().density + 0.5f),
+                                            ViewGroup.LayoutParams.WRAP_CONTENT);
+                                    textLayout.setMargins(20, 10, 0, 0);
+                                    TextView prod_name = new TextView(root.getContext());
+                                    prod_name.setLayoutParams(textLayout);
+                                    prod_name.setText(name);
+                                    prod_name.setTextSize(16);
+                                    Typeface t = getResources().getFont(R.font.inter_bold);
+                                    prod_name.setTypeface(t);
+                                    prod_name.setLineHeight(50);
+
+                                    LinearLayout linear1 = new LinearLayout(root.getContext());
+                                    LinearLayout.LayoutParams linearlp1 = new LinearLayout.LayoutParams(
+                                            (int) (155 * root.getContext().getResources().getDisplayMetrics().density + 0.5f),
+                                            ViewGroup.LayoutParams.WRAP_CONTENT);
+                                    linear1.setOrientation(LinearLayout.HORIZONTAL);
+                                    linear1.setLayoutParams(linearlp1);
+
+                                    if (discount != 0) {
+                                        LinearLayout.LayoutParams textlp = new LinearLayout.LayoutParams(
+                                                ViewGroup.LayoutParams.WRAP_CONTENT,
+                                                ViewGroup.LayoutParams.WRAP_CONTENT);
+                                        textlp.setMargins(20, 0, 0, 0);
+
+                                        TextView prod_price = new TextView(root.getContext());
+                                        prod_price.setLayoutParams(textlp);
+                                        String p = String.valueOf(price);
+                                        prod_price.setText("$" + p);
+                                        prod_price.setTextColor(Color.BLACK);
+                                        prod_price.setTextSize(14);
+                                        Typeface t1 = getResources().getFont(R.font.inter_bold);
+                                        prod_price.setTypeface(t1);
+                                        prod_price.setPaintFlags(prod_price.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+
+                                        TextView discount_price = new TextView(root.getContext());
+                                        discount_price.setLayoutParams(textlp);
+                                        double newPrice = price - discount;
+                                        discount_price.setText("$" + df.format(newPrice));
+                                        discount_price.setTextColor(Color.parseColor("#ffd814"));
+                                        discount_price.setTextSize(14);
+                                        Typeface t2 = getResources().getFont(R.font.inter_bold);
+                                        discount_price.setTypeface(t2);
+
+                                        linear1.addView(prod_price);
+                                        linear1.addView(discount_price);
+                                    } else {
+                                        TextView prod_price = new TextView(root.getContext());
+                                        prod_price.setLayoutParams(textLayout);
+                                        String p = String.valueOf(price);
+                                        prod_price.setText("$" + p);
+                                        prod_price.setTextColor(Color.BLACK);
+                                        prod_price.setTextSize(14);
+                                        Typeface t2 = getResources().getFont(R.font.inter_bold);
+                                        prod_price.setTypeface(t2);
+                                        linear1.addView(prod_price);
+                                    }
+
+                                    linear.addView(card);
+                                    linear.addView(prod_name);
+                                    linear.addView(linear1);
+                                    flex.addView(linear);
                                 }
                             }, 0, 0, ImageView.ScaleType.FIT_CENTER, Bitmap.Config.ARGB_8888,
                                     new Response.ErrorListener() {
@@ -100,12 +185,19 @@ public class HomeFragment extends Fragment {
 
                             queue.add(request);
                         } else {
+                            LinearLayout linear = new LinearLayout(root.getContext());
+                            LinearLayout.LayoutParams linearlp = new LinearLayout.LayoutParams(ViewGroup.
+                                    LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                            linearlp.setMargins(0, 0, 0, 60);
+                            linear.setOrientation(LinearLayout.VERTICAL);
+                            linear.setLayoutParams(linearlp);
+
                             CardView card = new CardView(root.getContext());
                             LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.
                                     LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                            lp.setMargins(0, 0, 0, 60);
                             card.setLayoutParams(lp);
                             card.setRadius((int) (10 * root.getContext().getResources().getDisplayMetrics().density + 0.5f));
+
                             ImageView img = new ImageView(root.getContext());
                             img.setImageBitmap(ImageCache.GetProductImage(prod_id));
                             LinearLayout.LayoutParams lp1 = new LinearLayout.LayoutParams(
@@ -113,8 +205,71 @@ public class HomeFragment extends Fragment {
                                     (int) (190 * root.getContext().getResources().getDisplayMetrics().density + 0.5f));
                             img.setLayoutParams(lp1);
                             img.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
                             card.addView(img);
-                            flex.addView(card);
+
+                            LinearLayout.LayoutParams textLayout = new LinearLayout.LayoutParams(
+                                    (int) (155 * root.getContext().getResources().getDisplayMetrics().density + 0.5f),
+                                    ViewGroup.LayoutParams.WRAP_CONTENT);
+                            textLayout.setMargins(20, 10, 0, 0);
+                            TextView prod_name = new TextView(root.getContext());
+                            prod_name.setLayoutParams(textLayout);
+                            prod_name.setText(name);
+                            prod_name.setTextSize(16);
+                            Typeface t = getResources().getFont(R.font.inter_bold);
+                            prod_name.setTypeface(t);
+                            prod_name.setLineHeight(50);
+
+                            LinearLayout linear1 = new LinearLayout(root.getContext());
+                            LinearLayout.LayoutParams linearlp1 = new LinearLayout.LayoutParams(
+                                    (int) (155 * root.getContext().getResources().getDisplayMetrics().density + 0.5f),
+                                    ViewGroup.LayoutParams.WRAP_CONTENT);
+                            linear1.setOrientation(LinearLayout.HORIZONTAL);
+                            linear1.setLayoutParams(linearlp1);
+
+                            if (discount != 0) {
+                                LinearLayout.LayoutParams textlp = new LinearLayout.LayoutParams(
+                                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                                        ViewGroup.LayoutParams.WRAP_CONTENT);
+                                textlp.setMargins(20, 0, 0, 0);
+
+                                TextView prod_price = new TextView(root.getContext());
+                                prod_price.setLayoutParams(textlp);
+                                String p = String.valueOf(price);
+                                prod_price.setText("$" + p);
+                                prod_price.setTextColor(Color.BLACK);
+                                prod_price.setTextSize(14);
+                                Typeface t1 = getResources().getFont(R.font.inter_bold);
+                                prod_price.setTypeface(t1);
+                                prod_price.setPaintFlags(prod_price.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+
+                                TextView discount_price = new TextView(root.getContext());
+                                discount_price.setLayoutParams(textlp);
+                                double newPrice = price - discount;
+                                discount_price.setText("$" + df.format(newPrice));
+                                discount_price.setTextColor(Color.parseColor("#ffd814"));
+                                discount_price.setTextSize(14);
+                                Typeface t2 = getResources().getFont(R.font.inter_bold);
+                                discount_price.setTypeface(t2);
+
+                                linear1.addView(prod_price);
+                                linear1.addView(discount_price);
+                            } else {
+                                TextView prod_price = new TextView(root.getContext());
+                                prod_price.setLayoutParams(textLayout);
+                                String p = String.valueOf(price);
+                                prod_price.setText("$" + p);
+                                prod_price.setTextColor(Color.BLACK);
+                                prod_price.setTextSize(14);
+                                Typeface t2 = getResources().getFont(R.font.inter_bold);
+                                prod_price.setTypeface(t2);
+                                linear1.addView(prod_price);
+                            }
+
+                            linear.addView(card);
+                            linear.addView(prod_name);
+                            linear.addView(linear1);
+                            flex.addView(linear);
                         }
                     }
                 } catch(Exception e) {
@@ -138,6 +293,7 @@ public class HomeFragment extends Fragment {
                     for (int i = 0; i < response.length(); i++) {
                         JSONObject row = response.getJSONObject(i);
                         int prod_id = row.getInt("prod_id");
+//                        String name = row.getString("name");
                         if(ImageCache.GetProductImage(prod_id) == null) {
                             ImageRequest request = new ImageRequest(ServerUrls.getNewDropsImage(prod_id), new Response.Listener<Bitmap>() {
                                 @Override
@@ -164,6 +320,7 @@ public class HomeFragment extends Fragment {
                                     } else {
                                         flipper.startFlipping();
                                     }
+
                                 }
                             }, 0, 0, ImageView.ScaleType.FIT_CENTER, Bitmap.Config.ARGB_8888,
                                     new Response.ErrorListener() {
